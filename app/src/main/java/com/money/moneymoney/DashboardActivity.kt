d2a6855d4ce1ef12af6f7ff8414c1a29b76b6c2c
amaryedida <49619138+amaryedida@users.noreply.github.com>
2025-06-06 10:21:40 +0400

    app testing 1

--- app/src/main/java/com/money/moneymoney/DashboardActivity.kt
+++ app/src/main/java/com/money/moneymoney/DashboardActivity.kt
@@ -17,7 +17,7 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.ScrollView
 import android.view.MotionEvent
-import androidx.recyclerview.widget.RecyclerView
+
 
 class DashboardActivity : AppCompatActivity() {
 
